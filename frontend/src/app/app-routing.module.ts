8b33d5e41b8f674d020fa5f522bf458fec1ad4a3
Swathi04 <swathianandram@gmail.com>
2022-04-12 09:45:40 -0700

    UI login and homepage updates

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},  
+  {path:'signup',component:SignupComponent},
+  {path:'**', redirectTo:'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
