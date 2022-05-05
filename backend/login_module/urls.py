c854d75b8c9a175e0cd5918b34ca1f3b96dc0840
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-05 13:01:51 -0700

    Added Auth Logic in Project API

--- backend/login_module/urls.py
+++ backend/login_module/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from . views import user_signup, user_sigin, manager_signin
+from . views import user_signup, user_signin, manager_signin
 
 urlpatterns = [
     path('signup/', user_signup, name='user_signup'),
-    path('signin/', user_sigin, name='user_signin'),
+    path('signin/', user_signin, name='user_signin'),
     path('manager_signin/', manager_signin, name='manager_signin'),
 ]
