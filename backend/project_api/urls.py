aedc26d6738443450a70c021c86b0871edf034d4
harikan23 <36985848+harikan23@users.noreply.github.com>
2022-05-06 16:23:43 -0700

    API changes

--- backend/project_api/urls.py
+++ backend/project_api/urls.py
@@ -3,6 +3,6 @@ from .views import CustomersAPI,RoomDetails, ContactViews
 
 urlpatterns = [
    path('customers/', CustomersAPI.as_view(), name="customer-api-view"),
-   path('roomdetails/', RoomDetails.as_view(), name="room-api-view"),
+   path('roomdetails/<str:datestring>', RoomDetails.as_view(), name="room-api-view"),
    path('contacts/', ContactViews.as_view(), name="contact-api-view"),
 ]
