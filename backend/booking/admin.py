ae2aca58790fa4a372a913ca3640e554f9da3323
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-08 11:32:50 -0700

    Refactored Bookings App with Models

--- backend/booking/admin.py
+++ backend/booking/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Contact, Room, Booking
+from .models import Room, Booking
 
 # Register your models here.
-admin.site.register(Contact)
 admin.site.register(Room)
 admin.site.register(Booking)
--- backend/booking/models.py
+++ backend/booking/models.py
@@ -1,27 +1,26 @@
 from django.db import models
-from accounts.models import Customer, RoomManager
 from datetime import date
 from django.contrib.auth.models import User
+from multiselectfield import MultiSelectField
 
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    message = models.TextField(max_length=2000)
-
-    def __str__(self):
-        return self.name
+Amenities_Choices = (
+    ("Daily Continental Breakfast", "Daily Continental Breakfast"),
+    ("Access to fitness room", "Access to fitness room"),
+    ("Access to Swimming Pool and Jacuzzi", "Access to Swimming Pool and Jacuzzi"),
+    ("Daily Parking", "Daily Parking"),
+    ("All meals included", "All meals included")
+)
 
 
 class Room(models.Model):
-    manager = models.ForeignKey(RoomManager, on_delete=models.CASCADE)
     room_no = models.CharField(max_length=5)
     room_type = models.CharField(max_length=50)
+    room_amenities = MultiSelectField(choices=Amenities_Choices, default="Daily Parking")
     is_available = models.BooleanField(default=True)
     price = models.FloatField(default=300.00)
     no_of_days_advance = models.IntegerField()
-    start_date = models.DateField(auto_now=False,
-                                  auto_now_add=False)
+    start_date = models.DateField(auto_now=True, auto_now_add=False)
+    room_location = models.CharField(max_length=50, default="sanjose")
     room_image = models.CharField(max_length=500)
 
     def __str__(self):
@@ -31,6 +30,9 @@ class Room(models.Model):
 class Booking(models.Model):
     room_no = models.ForeignKey(Room, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    number_of_guests = models.IntegerField(default=1)
+    booking_amenities = MultiSelectField(choices=Amenities_Choices,
+                                         default="Daily Parking")
     start_day = models.DateField(auto_now=False, auto_now_add=False)
     end_day = models.DateField(auto_now=False, auto_now_add=False)
     amount = models.FloatField()
