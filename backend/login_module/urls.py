eb57c2304714ad56e0a47f73b839c9942690c45e
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-08 11:34:21 -0700

    Refactored Project Files with First Deployed Version

--- backend/login_module/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . views import user_signup, user_signin, manager_signin
-
-urlpatterns = [
-    path('signup/', user_signup, name='user_signup'),
-    path('signin/', user_signin, name='user_signin'),
-    path('manager_signin/', manager_signin, name='manager_signin'),
-]
