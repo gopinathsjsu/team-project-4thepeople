3af7d879e1d5557a789da6ddf44446370232df95
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-09 14:42:57 -0700

    Updated Projects API Views

--- backend/project_api/urls.py
+++ backend/project_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SearchRoom, BookRoom
+from .views import SearchRoom, BookRoom, BookingDetails
 
 urlpatterns = [
    path('search/', SearchRoom.as_view(), name="search-api-view"),
    path('booking/', BookRoom.as_view(), name="book-api-view"),
+   path('booking_details/', BookingDetails.as_view(), name="booking-details-api-view"),
 ]
