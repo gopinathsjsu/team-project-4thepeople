4ed8ee49f00498a4cdfdd58a456462f6df7f704c
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-08 17:06:16 -0700

    Added bookings API Logic

--- backend/project_api/urls.py
+++ backend/project_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomDetails, SearchRoom, BookRoom
+from .views import SearchRoom, BookRoom
 
 urlpatterns = [
    path('search/', SearchRoom.as_view(), name="search-api-view"),
