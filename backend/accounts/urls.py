a764d872991e5d9c7d3e45142cb1087d3677ded5
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-10 08:59:27 -0700

    Edited Bookings CRUD Methods for Employees

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,12 +3,16 @@ from . import views
 from .views import UsersRegistrationAPI, UsersSigninAPI, ManageHotelAccountView, UsersProfileAPI
 
 urlpatterns = [
+    path('user/', views.userPage, name="user-page"),
     path('login/', views.loginPage, name="login"),
     path('register/', views.registerPage, name="register"),
     path('logout/', views.logoutUser, name="logout"),
 
     path('', views.home, name="home"),
-    path('user/', views.userPage, name="user-page"),
+    path('bookings/', views.bookings_list, name="booking_list"),
+    path('bookings/create/', views.create_booking, name='create_booking'),
+    path('bookings/edit/<str:pk>/', views.edit_booking, name='edit_booking'),
+    path('bookings/delete/<str:pk>/', views.delete_booking, name='delete_booking'),
 
     # authentication api
     path('api/register/', UsersRegistrationAPI.as_view(), name="customer_registration_api"),
