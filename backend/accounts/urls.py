7e4beeb7fc9bf01a93a832ed7c1ff97628683b15
Aryan Jadon <aryan.jadon@sjsu.edu>
2022-05-10 16:19:49 -0700

    Updated User Forms Styling for Dashboard

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,11 +8,24 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('logout/', views.logoutUser, name="logout"),
 
+    # bookings
     path('', views.home, name="home"),
     path('bookings/', views.bookings_list, name="booking_list"),
     path('bookings/create/', views.create_booking, name='create_booking'),
     path('bookings/edit/<str:pk>/', views.edit_booking, name='edit_booking'),
     path('bookings/delete/<str:pk>/', views.delete_booking, name='delete_booking'),
+
+    # rooms
+    path('rooms/', views.rooms_list, name="room_list"),
+    path('rooms/create/', views.create_room, name='create_room'),
+    path('rooms/edit/<str:pk>/', views.edit_room, name='edit_room'),
+    path('rooms/delete/<str:pk>/', views.delete_room, name='delete_room'),
+
+    # users
+    path('customers/', views.customers_list, name="customer_list"),
+    path('customers/create/', views.create_customer, name='create_customer'),
+    path('customers/edit/<str:pk>/', views.edit_customer, name='edit_customer'),
+    path('customers/delete/<str:pk>/', views.delete_customer, name='delete_customer'),
 
     # authentication api
     path('api/register/', UsersRegistrationAPI.as_view(), name="customer_registration_api"),
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -6,9 +6,11 @@ from django.contrib.auth.models import Group
 from .forms import CreateUserForm
 from .decorators import unauthenticated_user, admin_only
 from accounts.models import UserProfile
+from accounts.forms import CreateUserForm
 from booking.models import Booking, Room
+from booking.forms import BookingForm, RoomForm
 from django.contrib.auth.models import User
-from .serializer import AccountSerializer
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -62,13 +64,15 @@ def logoutUser(request):
     return redirect('login')
 
 
+########################################################################################################################
+
 @login_required(login_url='login')
 @admin_only
 def home(request):
     bookings_count = Booking.objects.count()
     rooms_count = Room.objects.count()
     upcoming_revenue = 0
-    package_types = {"Delux": 0, "Suite": 0, "Studio": 0}
+    package_types = {"Deluxe": 0, "Suite": 0, "Studio": 0}
     for booking in Booking.objects.all():
         if booking.booking_room_type in package_types:
             package_types[booking.booking_room_type] += 1
@@ -81,21 +85,31 @@ def home(request):
                "rooms_count": rooms_count,
                "upcoming_revenue": upcoming_revenue,
                "Studio": package_types["Studio"],
-               "StudioCount": int(package_types["Studio"] / rooms_count),
                "Suite": package_types["Suite"],
-               "SuiteCount": int(package_types["Suite"] / rooms_count),
-               "Delux": package_types["Delux"],
-               "DeluxCount": int(package_types["Delux"] / rooms_count),
+               "Deluxe": package_types["Deluxe"],
                }
     return render(request, 'hotel/dashboard.html', context)
 
 
+########################################################################################################################
+# Bookings
+
 @login_required(login_url='login')
 @admin_only
 def bookings_list(request):
-    bookings = Booking.objects.all()
+    search_query = ""
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+
+    bookings = Booking.objects.filter(
+        Q(booking_location__contains=search_query),
+        Q(booking_room_type__contains=search_query)
+    )
+
     context = {
         'bookings': bookings,
+        'search_query': search_query,
     }
     return render(request, 'hotel/booking_list.html', context)
 
@@ -103,24 +117,217 @@ def bookings_list(request):
 @login_required(login_url='login')
 @admin_only
 def create_booking(request):
-    context = {}
+    form = BookingForm()
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('bookings_list')
+        else:
+            print(form.errors)
+            messages.info(request, str(form.errors))
+
+    context = {
+        'form': form,
+    }
+
     return render(request, 'hotel/booking_create.html', context)
 
 
 @login_required(login_url='login')
 @admin_only
 def edit_booking(request, pk):
-    context = {}
+    booking = Booking.objects.get(id=pk)
+    form = BookingForm(instance=booking)
+
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('bookings_list')
+
+    context = {
+        'booking': booking,
+        'form': form,
+    }
+
     return render(request, 'hotel/booking_edit.html', context)
 
 
 @login_required(login_url='login')
 @admin_only
 def delete_booking(request, pk):
-    context = {}
+    booking = Booking.objects.get(id=pk)
+
+    if request.method == 'POST':
+        booking.delete()
+        return redirect('booking_list')
+
+    context = {
+        'booking': booking,
+    }
     return render(request, 'hotel/booking_delete.html', context)
 
 
+########################################################################################################################
+# Rooms
+
+@login_required(login_url='login')
+@admin_only
+def rooms_list(request):
+    search_query = ""
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+
+    rooms = Room.objects.filter(
+        Q(room_no__contains=search_query),
+        Q(room_type__contains=search_query)
+    )
+
+    context = {
+        'rooms': rooms,
+        'search_query': search_query,
+    }
+    return render(request, 'hotel/room_list.html', context)
+
+
+@login_required(login_url='login')
+@admin_only
+def create_room(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('room_list')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'hotel/room_create.html', context)
+
+
+@login_required(login_url='login')
+@admin_only
+def edit_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('room_list')
+
+    context = {
+        'rooms': room,
+        'form': form,
+    }
+
+    return render(request, 'hotel/room_edit.html', context)
+
+
+@login_required(login_url='login')
+@admin_only
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('room_list')
+
+    context = {
+        'room': room,
+    }
+    return render(request, 'hotel/room_delete.html', context)
+
+
+########################################################################################################################
+# Customers
+
+@login_required(login_url='login')
+@admin_only
+def customers_list(request):
+    search_query = ""
+
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+
+    customers = UserProfile.objects.filter(
+        Q(user_level__contains=search_query),
+        Q(total_rewards__contains=search_query)
+    )
+
+    context = {
+        'customers': customers,
+        'search_query': search_query,
+    }
+    return render(request, 'hotel/customer_list.html', context)
+
+
+@login_required(login_url='login')
+@admin_only
+def create_customer(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('customer_list')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'hotel/customer_create.html', context)
+
+
+@login_required(login_url='login')
+@admin_only
+def edit_customer(request, pk):
+    user_id = User.objects.get(id=pk)
+    user = UserProfile.objects.get(id=user_id.id)
+    form = RoomForm(instance=user)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('room_list')
+
+    context = {
+        'user': user,
+        'form': form,
+    }
+
+    return render(request, 'hotel/customer_edit.html', context)
+
+
+@login_required(login_url='login')
+@admin_only
+def delete_customer(request, pk):
+    customer = User.objects.get(id=pk)
+
+    if request.method == 'POST':
+        customer.delete()
+        return redirect('customer_list')
+
+    context = {
+        'customer': customer,
+    }
+
+    return render(request, 'hotel/customer_delete.html', context)
+
+
+########################################################################################################################
 @login_required(login_url='login')
 def userPage(request):
     context = {}
@@ -207,6 +414,33 @@ class UsersProfileAPI(APIView):
 
 class ManageHotelAccountView(APIView):
     # permission_classes = (IsAuthenticated,)
+    @staticmethod
+    def delete(request, *args, **kwargs):
+        try:
+            user_name = request.POST.get('username')
+            reward_points_booking = request.POST.get('reward_points')
+            user_detail = User.objects.filter(username=user_name)[0]
+            user_profile = UserProfile.objects.filter(user=user_detail)[0]
+            user_profile.total_bookings -= 1
+            user_profile.total_rewards -= int(reward_points_booking)
+
+            if 5 < user_profile.total_bookings < 10:
+                user_profile.user_level = "Gold"
+            elif user_profile.total_bookings > 10:
+                user_profile.user_level = "Diamond"
+
+            user_profile.save()
+
+            return Response({
+                "status": "success",
+                "message": "Account Details Modified Successfully"},
+                status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({
+                "status": "error",
+                "message": str(e)},
+                status=status.HTTP_400_BAD_REQUEST)
+
     @staticmethod
     def post(request, *args, **kwargs):
         try:
